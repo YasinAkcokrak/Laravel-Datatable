1da7d112f41d96b74dacc035808bd4fdc6318571
YasinAkcokrak <akcokrakyasin@gmail.com>
2022-03-17 15:37:04 -0400

    17 march 2022

--- resources/views/dataset.blade.php
+++ resources/views/dataset.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
 
     <title>Test</title>
-
+ <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/jquery.dataTables.min.js" integrity="sha512-BkpSL20WETFylMrcirBahHfSnY++H2O1W+UnEEO4yNIl+jI2+zowyoGJpbtk6bx97fBXf++WJHSSK2MV4ghPcg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ Route::get('/','App\Http\Controllers\DataController@index');
 
 
 Route::get('/getDataTable','App\Http\Controllers\DataController@getDataInToTable');
+
+
+Route::get('client',[ClientController::class,'index'])->name('client');
+Route::get('client/create',[ClientController::class,'create'])->name('client.create');
+Route::post('client/store',[ClientController::class,'store'])->name('client.store');
